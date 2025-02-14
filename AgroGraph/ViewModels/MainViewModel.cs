9c7ea69c5cbcb388e1d4df94634edc64eab80d98
daMpoff <11projectone@gmail.com>
2025-02-15 00:15:37 +0300

    fix: colors

--- AgroGraph/ViewModels/MainViewModel.cs
+++ AgroGraph/ViewModels/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Windows;
 using System.Windows.Input;
 using AgroGraph.Commands;
 using AgroGraph.Models;
